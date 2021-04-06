34096a697709e81a85e25c873cb6b2ada74429cc
wainer97 <wainer.oliveira.luiz@gmail.com>
2021-04-06 21:50:06 +0000

    .

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,21 +1,25 @@
-// useState: greeting
-// http://localhost:3000/isolated/exercise/01.js
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  // 🐨 initialize the state to the value from localStorage
+  // 💰 window.localStorage.getItem('name') || initialName
+  const [name, setName] = React.useState(initialName)
+
+  // 🐨 Here's where you'll use `React.useEffect`.
+  // The callback should set the `name` in localStorage.
+  // 💰 window.localStorage.setItem('name', name)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
-
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,7 +6,17 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+
+  // LAZY INITTIALIZER: inicialização "preguiçoso"
+  // Quando o useState recebe uma função em vez de um valor como estado inicial,
+  // essa função é executada apenas na fase mount do componente, sem se
+  // repetir na fase update.
+  const [name, setName] = React.useState(() => 
+    window.localStorage.getItem('name') || initialName)
+  const [count, setCount] = React.useState(0)
+  const [nameUC, setNameUC] = React.useState(() => 
+    window.localStorage.getItem('nameUC') || initialName)
+
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
@@ -15,13 +25,32 @@ function Greeting({initialName = ''}) {
   function handleChange(event) {
     setName(event.target.value)
   }
+
+  function handleClick(event) {
+    setNameUC(event.target.value.toUpperCase())
+  }
+
+  // Efeito colateral a ser executado após o componente ter sido atualizado.
+  React.useEffect(() => {
+      // O valor do localStorage será atualizado após a atualização do componente.
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('nameUC', nameUC)
+      setCount(count + 1)
+  }, [name, nameUC]) // [] é a lista de dependências, ou seja, esse useEffect é para ser chamado 
+  // quando a variável name ou nameUC sofrer alteração.
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} onClick={handleClick} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name} , {nameUC}</strong> : 'Please type your name'}
+      <p>
+          localStorage: {window.localStorage.getItem('name')} - 
+          {window.localStorage.getItem('nameUC')}
+      </p>
+      <p>Contagem: {count}</p>
     </div>
   )
 }
